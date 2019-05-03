9696163dfe8eaa8b7f8a35f0175e29eea58af194
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-05-03 23:44:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -140,7 +140,7 @@ end
 def team_colors(team_name)
   game_hash.each do |location, team_data|
     if team_data[:team_name] == team_name
-      return team_data[:colors]
+      puts team_data[:colors]
     end
   end
 end
