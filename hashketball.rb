cedd5e7036b878b49dad59864a78bed2c36a6a24
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-05-03 23:10:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,31 @@
 # Write your code here!
-
+def game_hash()
+  game_hash{
+    :home => {
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
+      :players => {
+        :playername => {
+        :number => 1,
+        :shoe => 1,
+        :points => 1,
+        :rebounds => 1,
+        :assists => 1,
+        :steals => 1,
+        :blocks => 1,
+        :slam_dunks 1,
+        },
+      },  
+    },
+    :away => {
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
+      :players => {
+        
+      },
+    },
+  }
+end
 
 
 
