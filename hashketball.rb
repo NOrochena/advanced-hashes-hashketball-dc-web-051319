45188be2d6134a3fc5fddd007a1993f38b17466b
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-05-03 23:31:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -120,7 +120,7 @@ end
 def num_points_scored(player_name)
   
   game_hash.each do |location, team_data|
-    team_data[:players].each do |player, stats|
+    location[:players].each do |player, stats|
       if player == player_name
         return 
       end
