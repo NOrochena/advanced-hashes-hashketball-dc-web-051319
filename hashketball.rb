dc4d26e0aafe45f2f14c8db8e0b3ab67dcfd2137
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-05-03 23:12:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,13 +5,13 @@ def game_hash()
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
       :players => {
-        :playername => {
-          :number => 1,
-          :shoe => 1,
-          :points => 1,
-          :rebounds => 1,
-          :assists => 1,
-          :steals => 1,
+        "Alan Anderson" => {
+          :number => 0,
+          :shoe => 16,
+          :points => 22,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 3,
           :blocks => 1,
           :slam_dunks 1,
         },
